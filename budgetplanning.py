e265f20d78ab26e3b544079fd82717b9757d793b
LucianoBeltrao <lucianoalvesjf@gmail.com>
2023-05-12 20:37:24 +0000

    Arquivo python atualizado com as bibliotecas

---
+++ budgetplanning.py
@@ -0,0 +1,20 @@
+
+#BudgetPlanning
+from dash import html, dcc
+import dash
+from dash.dependencies import Input, Output
+import dash_booststrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+from app import *
+
+content = html.Div(id="page-content")
+
+app.layout = dbc.Container(children=[
+    
+    
+    
+    
+    
+], fluid=True,)
