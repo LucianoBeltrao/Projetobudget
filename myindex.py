4af68b183a5f6da1d1d6c4d942f6b8ed59881e1f
LucianoBeltrao <lucianoalvesjf@gmail.com>
2023-05-12 21:10:39 +0000

    Criação do arquivo componnents e do app servidos

--- myindex.py
+++ myindex.py
@@ -3,11 +3,12 @@
 from dash import html, dcc
 import dash
 from dash.dependencies import Input, Output
-import dash_booststrap_components as dbc
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
 from app import *
+from components impor sidebar
 
 
 #=============Layout===============#
@@ -15,6 +16,13 @@ from app import *
 content = html.Div(id="page-content")
 
 app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout
+        ])
+    ])   
+
     
     
     
